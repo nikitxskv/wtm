1fea4ac2872fe524785e6da0540ba82d5060e2ce
nikitxskv <dmitnikita@ya.ru>
2015-04-17 12:14:16 +0300

    Add acmp_186

--- acmp/task186/p1.cpp
+++ acmp/task186/p1.cpp
@@ -3,20 +3,22 @@
 #include <vector>
 using namespace std;
 int max(int a,int b){return a>b?a:b;}
+int min(int a,int b){return a<b?a:b;}
 int main()
 {
 	FILE *f=fopen("INPUT.TXT","r"),*g=fopen("OUTPUT.TXT","w");
-	int n,x,cr,cl,min=0;
+	int n;
 	fscanf(f,"%d",&n);
-	vector<int> v(n);
+	vector<int> v(n),u(n);
 	for(int i=0;i<n;i++) fscanf(f,"%d",&v[i]);
 	sort(v.begin(),v.end());
-	cl=v[1]-v[0];
-	for(int i=2;i<n-1;i++)
+	for(int i=1;i<n;i++)
 	{
-		
+		if(i<3) u[i]=v[i]-v[0];
+		else u[i]=min(max(u[i-2],v[i]-v[i-1]),max(u[i-3],v[i]-v[i-2]));
 	}
-	min=max(min,v[v.size()-1]-cb);
-	fprintf(g,"%d",min);
+	fprintf(g,"%d\n",u.back());
+	fclose(f);
+	fclose(g);
 	return 0;
 } 
